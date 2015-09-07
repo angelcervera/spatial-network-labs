a7ffb38c97b1d482b0f53d3e9c7ee9ae8bb8564d
angelcervera <angelcervera@silyan.com>
2015-09-07 20:53:07 +0100

    Model must be agnostic and no dependent of OSM

---
+++ algorithms/drivingDistance/src/main/scala/com/acervera/spatialnetworklabs/core/algorithms/drivingdistance/DrivingDistance.scala
@@ -0,0 +1,32 @@
+package com.acervera.spatialnetworklabs.core.algorithms.drivingdistance
+
+import com.acervera.spatialnetworklabs.core.{Vertex, GraphStorage}
+
+/**
+ * Implementation of the Driving Distance algorithm, used to create spanning trees of coverage.
+ */
+class DrivingDistance(storage: GraphStorage) {
+
+  /**
+   * Starting from the source vertex, calculate all the spanning tree up the nodes with a cost to arrive near or equal
+   * to "weight", but only return the set of vertexes that are in the perimeter (the last node in every path).
+   * Edges must have two attributes:
+   * - "costBackward"
+   * - "costForward"
+   *
+   * @param source Origin from will start.
+   * @param weight List of weights.
+   * @return A map with one set of Vertexes (the value) per weight (the key).
+   */
+  def start(source: Vertex, weight: Long*): Map[Long, Set[Vertex]] = {
+
+    // TODO: costBackward and costForward keys should be parameterizable.
+
+    // TODO: Implement. At the moment, Mocked
+    Map(1L -> Set(
+      new Vertex(0, 0, 0, "label", Map("prop1" -> "val1", "prop2" -> 3d)),
+      new Vertex(1, 1, 1, "label", Map())
+    ))
+  }
+
+}
